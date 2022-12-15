9dd174a0cba2b55dba0507307ac40f102546ce8a
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-16 04:34:24 +0800

    g

--- server.js
+++ server.js
@@ -34,15 +34,60 @@ const userSchema = {
 
 const User = mongoose.model("User", userSchema);
 
+Promise.all([
+  // load all the models first before anything
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+  fetchWorkouts(),
+])
+
+async function loadLabeledImages() {
+  const labels = await User.map((user) => {
+    return user.username;
+  }); 
+
+  console.log(labels);
+
+  try {
+    return Promise.all(
+      labels.map(async (label) => {
+        const descriptions = [];
+        // for (let i = 1; i <= 1; i++) {
+        const img = await faceapi.fetchImage(
+          `https://res.cloudinary.com/durortebu/image/upload/v1/photos/${label}/1.jpg`
+        );
+        console.log(
+          `https://res.cloudinary.com/durortebu/image/upload/v1/photos/${label}/1.jpg`
+        );
+        // text += label+"\n";
+        // document.getElementById('text').innerHTML= text;
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        c('prob')
+        descriptions.push(detections.descriptor);
+
+        c("check images");
+        // }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // get all user
 app.get("/", async (req, res) => {
   const users = await User.find({}).sort({ updatedAt: -1 });
   res.status(200).json(users);
 });
+
 app.get("/hello", async (req, res) => {
-  const users = {
-    "message": "hello"
-  }
+  const users = await User.find({username})
   res.status(200).json(users);
 });
 
