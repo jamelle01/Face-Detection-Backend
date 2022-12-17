2211adc501b9bb56ee8ddc4ad9b5467e221cca62
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-12-17 11:32:54 +0800

    reverse

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const port = process.env.PORT || 4044;
-
+const port = process.env.PORT || 4044; // where the server run
 app.use(cors());
 
 mongoose
@@ -34,60 +33,8 @@ const userSchema = {
 
 const User = mongoose.model("User", userSchema);
 
-Promise.all([
-  // load all the models first before anything
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  fetchWorkouts(),
-])
-
-async function loadLabeledImages() {
-  const labels = await User.map((user) => {
-    return user.username;
-  }); 
-
-  console.log(labels);
-
-  try {
-    return Promise.all(
-      labels.map(async (label) => {
-        const descriptions = [];
-        // for (let i = 1; i <= 1; i++) {
-        const img = await faceapi.fetchImage(
-          `https://res.cloudinary.com/durortebu/image/upload/v1/photos/${label}/1.jpg`
-        );
-        console.log(
-          `https://res.cloudinary.com/durortebu/image/upload/v1/photos/${label}/1.jpg`
-        );
-        // text += label+"\n";
-        // document.getElementById('text').innerHTML= text;
-        const detections = await faceapi
-          .detectSingleFace(img)
-          .withFaceLandmarks()
-          .withFaceDescriptor();
-        c('prob')
-        descriptions.push(detections.descriptor);
-
-        c("check images");
-        // }
-        return new faceapi.LabeledFaceDescriptors(label, descriptions);
-      })
-    );
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 // get all user
 app.get("/", async (req, res) => {
   const users = await User.find({}).sort({ updatedAt: -1 });
   res.status(200).json(users);
 });
-
-app.get("/hello", async (req, res) => {
-  const users = await User.find({username})
-  res.status(200).json(users);
-});
-
