15944174861b924013342f9d41fec9a94f93ec2e
ericmaglajos <ercmgljs.mericle@gmail.com>
2023-01-02 14:49:09 +0800

    g

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post("/post", async (req, res) => {
 
     if (resObj.errorCode === 0) {
       // authorized successfully
-      res.send({ message: "success" });
+      res.status(200).json({message:"success"});
     } else {
       // authorization failed
       res.send({ message: "failure" });
