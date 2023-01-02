52bda8904b1d52d83fae62d62a89b364e34229d0
ericmaglajos <ercmgljs.mericle@gmail.com>
2023-01-02 11:33:24 +0800

    final na

--- server.js
+++ server.js
@@ -40,3 +40,67 @@ app.get("/", async (req, res) => {
 
   res.status(200).json(users);
 });
+app.post("/post", async (req, res) => {
+  const { clientMac, apMac, ssidName, radioId } = req.body;
+
+  const authInfo = {
+    clientMac: clientMac,
+    apMac: apMac,
+    ssidName: ssidName,
+    radioId: radioId,
+    authType: 4
+  };
+  // const csrfToken = getCSRFToken();
+
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    // "Csrf-Token": csrfToken,
+  };
+
+  const xhr = new XMLHttpRequest();
+
+  // post
+  xhr.open(
+    "POST",
+    `https://192.168.0.115:8043/53477786b5ff63adf8978a17cb6d79c6/api/v2/hotspot/login`
+  );
+
+  // Set return to a value, not return to page
+  xhr.responseType = "text";
+
+  // Set up cookies.
+  xhr.withCredentials = true;
+
+  // Allow Self Signed Certs
+  xhr.rejectUnauthorized = false;
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Accept", "application/json");
+  // xhr.setRequestHeader("Csrf-Token", csrfToken);
+
+  // API Call
+  xhr.send(JSON.stringify(authInfo));
+
+  xhr.addEventListener("load", () => {
+    const res = xhr.responseText;
+    console.log(res);
+    const resObj = JSON.parse(res);
+
+    if (resObj.errorCode === 0) {
+      // authorized successfully
+    }
+  });
+});
+
+// function getCSRFToken() {
+//   const fs = new FileSystem();
+//   const file = fs.openFile(TOKEN_FILE_PATH, { create: true });
+//   if (!file) {
+//     console.error("Unable to open file!");
+//     return;
+//   }
+//   const token = file.read();
+//   file.close();
+//   return token;
+// }
